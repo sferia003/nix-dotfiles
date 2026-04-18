62ea93a2d272f9fc06b1f3d6a0452b7c64eb4f9b
Stephen Feria <sferia003@gmail.com>
2026-04-17 19:31:30 -0700

    Revert scroll direction for mac

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -5,6 +5,8 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
+  system.primaryUser = username;
+
   users.users.${username} = {
     home = "/Users/${username}";
   };
@@ -12,6 +14,8 @@
   # Mac/system-level things
   programs.zsh.enable = true;
   programs.fish.enable = true;
+
+  system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
 
   environment.systemPackages = with pkgs; [
     vim
